670200c63d5f4e64c7b8a059506472004d2f597a
山本憲 <em072029@gmail.com>
2018-03-20 00:25:14 +0900

    Modify-sample

--- examples/example.rb
+++ examples/example.rb
@@ -1,15 +1,15 @@
 require_relative '../lib/crypto_unit'
 
 # BTC UNIT
-p 5.satoshi
+p 5.satoshi.to_f
 
 # ETHER UNIT
-p 3.ether
+p 3.ether.to_f
 
-p 2.finney
+p 2.finney.to_f
 
 # LTC unit
 
-p 3.lites
+p 3.lites.to_f
 
-p 5.photones
+p 5.photones.to_f
